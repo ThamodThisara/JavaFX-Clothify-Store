16985daa4a1e690856fa6089b562cbf5abdac15e
ThamodThisara <thamodthisara99@gmail.com>
2024-10-11 20:25:54 +0530

    first commit

---
+++ src/main/java/controller/DashFormController.java
@@ -0,0 +1,25 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class DashFormController {
+
+    @FXML
+    private TextField txtPassword;
+
+    @FXML
+    private TextField txtUserName;
+
+    @FXML
+    void btnLoginOnAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnSignUpOnAction(MouseEvent event) {
+
+    }
+
+}
